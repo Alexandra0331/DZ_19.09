0901ecf49c1f316327036d652ac239de3ea5375b
Alexandra <Kaverzina0331@mail.ru>
2022-09-22 18:14:34 +0300

    Задача 6

---
+++ Задача 6/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите значение: ");
+int n = Convert.ToInt32(Console.ReadLine());
+// 6. Напишите программу, которая на вход принмает число и
+// выдает, является ли число четным (делится ли оно на два без остатка).
+// 4 -> yes
+// - 3 -> no
+// 7 -> no
+if (n % 2 == 0)
+{
+  Console.WriteLine ("yes");
+}
+else
+ Console.WriteLine ("no");   
